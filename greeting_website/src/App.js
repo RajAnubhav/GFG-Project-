b0d2276e7e068617b8cf0b5e5c3373a694e97c09
RajAnubhav <anubhavraj742@gmail.com>
2022-09-10 22:07:40 +0530

    adding files in react and nodejs

---
+++ greeting_website/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>Welcome to the greeting website
+    </div>
+  );
+}
+
+export default App;
